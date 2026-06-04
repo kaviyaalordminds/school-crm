12e3c198583a17119aaa1800f85abb9705fa0d87
kaviyaalordminds <kaviyaalordminds@gmail.com>
2026-06-04 18:29:36 +0530

    Add files via upload

---
+++ config/app.php
@@ -0,0 +1,15 @@
+<?php
+define(
+    "BASE_URL",
+    "http://localhost/school-crm/"
+);
+
+define(
+    "APP_NAME",
+    "FutureEdu ERP"
+);
+
+date_default_timezone_set(
+    "Asia/Kolkata"u
+);
+?>
